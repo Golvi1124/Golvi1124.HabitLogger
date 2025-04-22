f0124d0015eed4feed72aa02a1b2d6e573600628
Vita Gold <golvi1124@gmail.com>
2025-04-22 15:16:26 +0200

    AddRecord()

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// installing Spectre Console NuGet Package to easier to create beautiful, cross platform, console applications
+using System.Globalization;
 using Microsoft.Data.Sqlite;
 using Spectre.Console;
 
@@ -32,7 +33,7 @@ void MainMenu()
         switch (usersChoice)
         {
             case "Add Record":
-            //    AddRecord();
+                AddRecord();
                 break;
             case "Delete Record":
             //    DeleteRecord();
@@ -53,6 +54,63 @@ void MainMenu()
         }
     }
 }
+
+void AddRecord()
+{
+    string date = GetDate("\nEnter the date (format - dd-mm-yy) or insert 0 to Go Back to Main Menu:\n");
+    int quantity = GetNumber("\nPlease enter number of meters walked (no decimals or negatives allowed) or enter 0 to go back to Main Menu.");
+
+    using (SqliteConnection connection = new(connectionString))
+    {
+        connection.Open();
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = $"INSERT INTO walkingHabit (date, meters) VALUES ('{date}', {quantity})";
+            command.ExecuteNonQuery(); 
+        }
+    }
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    string numberInput = Console.ReadLine();
+
+    if (numberInput == "0") MainMenu();
+    int output = 0;
+
+    while(!int.TryParse(numberInput, out output) || output < 0)
+    {
+        Console.WriteLine("\n\nInvalid number. Please try again!\n\n");
+        numberInput = Console.ReadLine();
+    }
+    return output;
+    
+}
+
+string GetDate(string message) //will use it display message to user about wanted input
+{
+    Console.WriteLine(message);
+    string dateInput = Console.ReadLine();
+
+    if (dateInput == "0") MainMenu(); //if input 0, return to menu
+
+    // returns input only if parsing successful. if incorrect, stays in the loop
+    while(!DateTime.TryParseExact(dateInput, "dd-MM-yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+    {
+        Console.WriteLine("\n\nInvalid date. (Format: dd-mm-yy). Please try again!\n\n");
+        dateInput = Console.ReadLine();
+    }
+    return dateInput; 
+}
+
+
+
+
+
+
+
 
 void CreateDatabase()
 {
