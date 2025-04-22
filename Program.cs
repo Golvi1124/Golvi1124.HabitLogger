27ed69311521f55c30cc018febd13f7bafa83b9c
Vita Gold <golvi1124@gmail.com>
2025-04-22 13:11:59 +0200

    added menu and switch

--- Program.cs
+++ Program.cs
@@ -1,8 +1,58 @@
-﻿using Microsoft.Data.Sqlite;
+﻿// installing Spectre Console NuGet Package to easier to create beautiful, cross platform, console applications
+using Microsoft.Data.Sqlite;
+using Spectre.Console;
 
-string connectionString = @"Data Source = habit-logger.db";
+string connectionString = @"Data Source = habit-logger.db"; // path to the database file. If it doesn't exist, it will be created in the same directory as the executable
 
 CreateDatabase();
+
+MainMenu();
+
+void MainMenu()
+{
+    var isMenuRunning = true;
+
+    while (isMenuRunning)
+    {   
+        // Spectre Connsole NuGet allows user to use arrows to select choices.
+        // Promt method returns a string which will be the user choice..stored in variable usersChoice
+        
+        var usersChoice = AnsiConsole.Prompt(     
+            new SelectionPrompt<string>()
+                .Title("What do you want to do?")
+                .AddChoices(
+                    "Add Record",
+                    "Delete Record",
+                    "View Records",
+                    "Update Record",
+                    "Quit"
+                )
+            );
+
+        switch (usersChoice)
+        {
+            case "Add Record":
+            //    AddRecord();
+                break;
+            case "Delete Record":
+            //    DeleteRecord();
+                break;
+            case "View Records":
+            //    ViewRecords();
+                break;
+            case "Update Record":
+             //   UpdateRecord();
+                break;
+            case "Quit":
+                Console.WriteLine("Goddbye!");
+                isMenuRunning = false;
+                break;
+            default: //since using Spectre consule, don't need necceserily to use default case, since user can't select anything else than the options
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
+    }
+}
 
 void CreateDatabase()
 {
@@ -16,8 +66,8 @@ void CreateDatabase()
                     Id INTEGER PRIMARY KEY AUTOINCREMENT,
                     Date TEXT, 
                     Quantity INTEGER
-                    )"; //TEXT (correct SQLite type) instead of TXT (which isn't standard)
-            tableCmd.ExecuteNonQuery();
+                    );"; //TEXT (correct SQLite type) instead of TXT (which isn't standard)
+            tableCmd.ExecuteNonQuery(); //call the ExecuteNonQuery method when we don't want any data to be returned
         }
     }
 }
