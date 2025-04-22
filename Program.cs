f7d08083f6f95a421faacc42ca11e223d0716c46
Vita Gold <golvi1124@gmail.com>
2025-04-22 12:10:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source = habit-logger.db";
+
+CreateDatabase();
+
+void CreateDatabase()
+{
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Open();
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS walkingHabit (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TXT,
+                    Meters INTEGER );";
+                   
+
+            command.ExecuteNonQuery();
+                
+        }
+    }
+}
