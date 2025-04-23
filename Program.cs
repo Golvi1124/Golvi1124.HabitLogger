0857c8bb1580b7119409d0895f4918967c5d96e3
Vita Gold <golvi1124@gmail.com>
2025-04-23 23:47:19 +0200

    Added ReadMe + general cleaning

--- Program.cs
+++ Program.cs
@@ -1,24 +1,4 @@
-﻿/* Rest of To Do:
- *  handle all possible errors so that the application never crashes.
- *  Follow the DRY Principle, and avoid code repetition - maybe can improve somethign in the end?
- *  Your project needs to contain a Read Me file where you'll explain how your app works. Here's a nice example: https://github.com/thags/ConsoleTimeLogger
- *  You can keep all of the code in one single class if you wish. - seperate methods in different files
- *  
- *  Extra challanges:
- *  If you haven't, try using parameterized queries to make your application more secure.
- *  https://reintech.io/blog/mastering-parameterized-queries-ado-net
- *  Let the users create their own habits to track. That will require that you let them choose the unit of measurement of each habit.
- *  Seed Data into the database automatically when the database gets created for the first time, generating a few habits and inserting a 
- hundred records with randomly generated values. This is specially helpful during development so you don't have to reinsert data every 
-time you create the database.
- *  Create a report functionality where the users can view specific information (i.e. how many times the user ran in a year? how many kms?) SQL allows you to ask very interesting things from your database.
- *  
- 
- */
-
-
-// installing Spectre Console NuGet Package to easier to create beautiful, cross platform, console applications
-using System.Globalization;
+﻿using System.Globalization;
 using Microsoft.Data.Sqlite;
 using Spectre.Console;
 
@@ -67,16 +47,84 @@ void MainMenu()
                 Console.WriteLine("Goddbye!");
                 isMenuRunning = false;
                 break;
-            default: //since using Spectre consule, don't need necceserily to use default case, since user can't select anything else than the options
-                Console.WriteLine("Invalid choice. Please try again.");
-                break;
+                // Because of using Spectre console, don't need to use default case, since user can't select anything else than the options
         }
     }
 }
 
+void CreateDatabase()
+{
+    {
+        using (SqliteConnection connection = new(connectionString))
+        using (SqliteCommand tableCmd = connection.CreateCommand())
+        {
+            connection.Open();
+            tableCmd.CommandText =
+                @"CREATE TABLE IF NOT EXISTS walkingHabit (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT, 
+                    Quantity INTEGER
+                    );"; //TEXT (correct SQLite type) instead of TXT (which isn't standard)
+            tableCmd.ExecuteNonQuery(); //call the ExecuteNonQuery method when we don't want any data to be returned
+        }
+    }
+}
+
+void AddRecord()
+{
+    string date = GetDate("\nEnter the date (format - dd-mm-yy) or insert 0 to Go Back to Main Menu:\n");
+    int quantity = GetNumber("\nPlease enter number of meters walked (no decimals or negatives allowed) or enter 0 to go back to Main Menu.");
+
+    using (SqliteConnection connection = new(connectionString))
+    using (SqliteCommand insertCmd = connection.CreateCommand())
+    {
+        connection.Open();
+        insertCmd.CommandText = $"INSERT INTO walkingHabit (date, meters) VALUES ('{date}', {quantity})";
+        insertCmd.ExecuteNonQuery();
+    }
+}
+
+void DeleteRecord()
+{
+    GetRecords(); // show the records to the user so they can select which one to delete
+
+    int id = GetNumber("\nPlease enter the ID of the record you want to delete.");
+
+    using (SqliteConnection connection = new(connectionString))
+    using (SqliteCommand deleteCmd = connection.CreateCommand())
+    {
+        connection.Open();
+        deleteCmd.CommandText = $"DELETE FROM walkingHabit WHERE Id = {id}"; // delete the record with the given id
+        int rowsAffected = deleteCmd.ExecuteNonQuery();
+        if (rowsAffected != 0)
+        {
+            Console.WriteLine("Record deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Record not found.");
+        }
+    }
+}
+
+void ViewRecords(List<WalkingRecord> records) // for visualizing the records in a table
+{
+    var table = new Table(); // Spectre Console table 
+    table.AddColumn("Id"); // table Class from Spectre Console
+    table.AddColumn("Date");
+    table.AddColumn("Amount");
+
+    foreach (var record in records)
+    {
+        table.AddRow(record.Id.ToString(), record.Date.ToString("dd-MM-yy"), record.Meters.ToString());
+    }
+
+    AnsiConsole.Write(table); // write the table to the console
+}
+
 void UpdateRecord()
 {
-    GetRecords(); 
+    GetRecords();
 
     int id = GetNumber("\nPlease enter the ID of the record you want to update.");
 
@@ -108,39 +156,12 @@ void UpdateRecord()
         query = $"UPDATE walkingHabit SET Meters = {quantity} WHERE Id = {id}";
     }
 
-    using (var connection = new SqliteConnection(connectionString))
+    using (SqliteConnection connection = new(connectionString))
+    using (SqliteCommand updateCmd = connection.CreateCommand())
     {
         connection.Open();
-        using (var command = connection.CreateCommand())
-        {
-            command.CommandText = query;
-            command.ExecuteNonQuery();
-        }
-    }
-}
-
-void DeleteRecord()
-{
-    GetRecords(); // show the records to the user so they can select which one to delete
-
-    int id = GetNumber("\nPlease enter the ID of the record you want to delete.");
-
-    using (var connection = new SqliteConnection(connectionString))
-    {
-        connection.Open();
-        using (var command = connection.CreateCommand())
-        {
-            command.CommandText = $"DELETE FROM walkingHabit WHERE Id = {id}"; // delete the record with the given id
-            int rowsAffected = command.ExecuteNonQuery();
-            if (rowsAffected != 0)
-            {
-                Console.WriteLine("Record deleted successfully.");
-            }
-            else
-            {
-                Console.WriteLine("Record not found.");
-            }
-        }
+        updateCmd.CommandText = query; // set the command text to the query
+        updateCmd.ExecuteNonQuery();
     }
 }
 
@@ -148,84 +169,62 @@ void GetRecords()
 {
     List<WalkingRecord> records = new(); // Representing rows in db
 
-    // creating an instance of the SQLiteDataReader class.
-    using (var connection = new SqliteConnection(connectionString)) // to read the data from the table
+    using (SqliteConnection connection = new(connectionString))
+    using (SqliteCommand getCmd = connection.CreateCommand())
     {
         connection.Open();
+        getCmd.CommandText = "SELECT * FROM walkingHabit"; // select all records from the table
 
-        using (var command = connection.CreateCommand())
+        using (var reader = getCmd.ExecuteReader()) //(SqliteDataReader reader = command.ExecuteReader())
         {
-            command.CommandText = "SELECT * FROM walkingHabit"; // select all records from the table
+            /*checking if the reader has rows and if it does we will read the data in a loop. 
+            For each row found in the table, we will add a new WalkingRecord to the list.*/
 
-            using (var reader = command.ExecuteReader()) //(SqliteDataReader reader = command.ExecuteReader())
+            if (reader.HasRows) // check if there are any records in the table
             {
-                /*checking if the reader has rows and if it does we will read the data in a loop. 
-                For each row found in the table, we will add a new WalkingRecord to the list.*/
-
-                if (reader.HasRows) // check if there are any records in the table
+                while (reader.Read())
                 {
-                    while (reader.Read())
+                    /*The code around the reading operation is wrapped by a try-catch block 
+                    to prevent the app from crashing in case the operation against the database goes wrong.*/
+                    try
                     {
-
-                        /*The code around the reading operation is wrapped by a try-catch block 
-                        to prevent the app from crashing in case the operation against the database goes wrong.*/
-                        try
-                        {
-                            records.Add(new WalkingRecord(
-                                reader.GetInt32(0), // Id
-                                DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", CultureInfo.InvariantCulture), // Date
-                                reader.GetInt32(2) // Meters
-                                )
-                             );
-                        }
-                        catch (FormatException ex)
-                        {
-                            Console.WriteLine($"Error reading record: {ex.Message}. Skipping this record.");
-                        }
+                        records.Add(new WalkingRecord(
+                            reader.GetInt32(0), // Id
+                            DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", CultureInfo.InvariantCulture), // Date
+                            reader.GetInt32(2) // Meters
+                            )
+                         );
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine($"Error reading record: {ex.Message}. Skipping this record.");
                     }
                 }
-                else
-                {
-                    Console.WriteLine("No records found.");
-                }
-
             }
+            else
+            {
+                Console.WriteLine("No records found.");
+            }
+
         }
     }
-
     ViewRecords(records);
 }
 
-void ViewRecords(List<WalkingRecord> records) // for visualizing the records in a table
+string GetDate(string message) //will use it display message to user about wanted input
 {
-    var table = new Table(); // Spectre Console table 
-    table.AddColumn("Id"); // table Class from Spectre Console
-    table.AddColumn("Date");
-    table.AddColumn("Amount");
+    Console.WriteLine(message);
+    string dateInput = Console.ReadLine();
 
-    foreach (var record in records)
+    if (dateInput == "0") MainMenu(); //if input 0, return to menu
+
+    // returns input only if parsing successful. if incorrect, stays in the loop
+    while (!DateTime.TryParseExact(dateInput, "dd-MM-yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
     {
-        table.AddRow(record.Id.ToString(), record.Date.ToString("dd-MM-yy"), record.Meters.ToString());
+        Console.WriteLine("\n\nInvalid date. (Format: dd-mm-yy). Please try again!\n\n");
+        dateInput = Console.ReadLine();
     }
-
-    AnsiConsole.Write(table); // write the table to the console
-}
-
-void AddRecord()
-{
-    string date = GetDate("\nEnter the date (format - dd-mm-yy) or insert 0 to Go Back to Main Menu:\n");
-    int quantity = GetNumber("\nPlease enter number of meters walked (no decimals or negatives allowed) or enter 0 to go back to Main Menu.");
-
-    using (SqliteConnection connection = new(connectionString))
-    {
-        connection.Open();
-
-        using (var command = connection.CreateCommand())
-        {
-            command.CommandText = $"INSERT INTO walkingHabit (date, meters) VALUES ('{date}', {quantity})";
-            command.ExecuteNonQuery();
-        }
-    }
+    return dateInput;
 }
 
 int GetNumber(string message)
@@ -245,46 +244,7 @@ int GetNumber(string message)
 
 }
 
-string GetDate(string message) //will use it display message to user about wanted input
-{
-    Console.WriteLine(message);
-    string dateInput = Console.ReadLine();
 
-    if (dateInput == "0") MainMenu(); //if input 0, return to menu
-
-    // returns input only if parsing successful. if incorrect, stays in the loop
-    while (!DateTime.TryParseExact(dateInput, "dd-MM-yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
-    {
-        Console.WriteLine("\n\nInvalid date. (Format: dd-mm-yy). Please try again!\n\n");
-        dateInput = Console.ReadLine();
-    }
-    return dateInput;
-}
-
-
-
-
-
-
-
-
-void CreateDatabase()
-{
-    using (SqliteConnection connection = new(connectionString)) //changed to C# 9+ shorthand syntax (before:  using (var connection = new SqliteConnection(connectionString)))
-    {
-        using (SqliteCommand tableCmd = connection.CreateCommand())
-        {
-            connection.Open(); //now comes after the command is created instead of before
-            tableCmd.CommandText = // 'tableCmd' is more descriptive than 'command'. no fifference in functionality
-                @"CREATE TABLE IF NOT EXISTS walkingHabit (
-                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    Date TEXT, 
-                    Quantity INTEGER
-                    );"; //TEXT (correct SQLite type) instead of TXT (which isn't standard)
-            tableCmd.ExecuteNonQuery(); //call the ExecuteNonQuery method when we don't want any data to be returned
-        }
-    }
-}
 // Record is a new C# 9 feature. It is a reference type that provides built-in functionality for encapsulating data.
 // It is immutable(cannot be changed after it is created) by default and provides value-based equality.
 record WalkingRecord(int Id, DateTime Date, int Meters);
---
+++ README.md
@@ -0,0 +1,62 @@
+# Habit Logger
+
+## Tasks for the project:  
+Using guidelines from The C# Academy course - [Link to page](https://thecsharpacademy.com/project/12/habit-logger)
+
+### Requirements:
+- [ ] Create a console application where you’ll log occurrences of a habit.
+- [x] This habit can't be tracked by time (ex. hours of sleep), only by quantity (ex. number of water glasses a day)
+- [x] Users need to be able to input the date of the occurrence of the habit
+- [x] The application should store and retrieve data from a real database
+- [x] When the application starts, it should create a sqlite database, if one isn’t present.
+- [x] It should also create a table in the database, where the habit will be logged.
+- [x] The users should be able to insert, delete, update and view their logged habit.
+- [ ] You should handle all possible errors so that the application never crashes.
+- [x] You can only interact with the database using ADO.NET. You can’t use mappers such as Entity Framework or Dapper.
+- [ ] Follow the DRY Principle, and avoid code repetition.
+- [ ] Your project needs to contain a Read Me file where you'll explain how your app works. Here's a nice example: https://github.com/thags/ConsoleTimeLogger
+
+### Challanges:
+- [ ] If you haven't, try using parameterized queries to make your application more secure. https://reintech.io/blog/mastering-parameterized-queries-ado-net
+- [ ] Let the users create their own habits to track. That will require that you let them choose the unit of measurement of each habit.
+- [ ] Seed Data into the database automatically when the database gets created for the first time,   
+generating a few habits and inserting a  hundred records with randomly generated values.  
+This is specially helpful during development so you don't have to reinsert data every time you create the database.
+- [ ] Create a report functionality where the users can view specific information (i.e. how many times the user ran in a year? how many kms?) SQL allows you to ask very interesting things from your database.
+
+
+
+
+
+
+
+
+
+## General To Do:
+- [ ] Handle all possible errors so that the application never crashes.
+- [ ] Follow the DRY Principle, and avoid code repetition - maybe can improve something in the end?
+- [ ] You can keep all of the code in one single class if you wish. - seperate methods in different files
+- [ ] Use the same structure and/or improve readability.
+
+
+## Extra challanges:
+
+
+- [ ] Add a feature that allows the user to set a goal for each habit. For example, if you want to run 5 times a week, you can set a goal of 5. The app will then track how many times you have run and how many times you have reached your goal.
+
+---
+### Improvements:
+- Installed **Spectre Console NuGet Package** to make it easier to create beautiful, cross platform, console applications.
+
+
+### Small extra improvements:
+- Using file-scoped namespaces to reduce nesting
+- Use same structure and/or improve readability.   
+	- 'using' declaration expression (C# 9+) for cleaner disposal  
+	>Before:   `using (var connection = new SqliteConnection(connectionString)))`  
+	After:    `using (SqliteConnection connection = new(connectionString))`  
+	- Make command more descriptive (changed depending on method)  
+	>Before:   `command.ExecuteNonQuery();`  = generic, but can be confusing if you have multiple commands  
+	After:    `deleteCmd.ExecuteNonQuery();` = more descriptive (better readability)  
+
+
